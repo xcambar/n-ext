dbb46df87292f3ee76739341386b3a865a4847ad
Xavier Cambar <xavier.cambar@inovia14.(none)>
2011-07-13 18:53:44 +0200

    [TECH] Started the implementation of a custom version of ExtJS for the server.

--- bootstrap.js
+++ bootstrap.js
@@ -2,7 +2,7 @@ require.paths.unshift('./lib');
 
 var sencha = require('n-ext'); 
 sencha.setDebug(true);
-sencha.setPath(__dirname + '/lib/Ext/src/');
+sencha.setPath(__dirname + '/lib/Ext-srv/');
 var E = sencha.bootstrapCore(true);
 //sencha.bootstrapCore(false); //with the param set to false, Ext is available app-wide
 
--- examples/NoSQL.js
+++ examples/NoSQL.js
@@ -1,16 +1,20 @@
 require.paths.unshift('./../lib');
 
 var sencha = require('n-ext'); 
-sencha.setPath(__dirname + '/../lib/Ext/src/');
+sencha.setPath(__dirname + '/../lib/Ext-srv/');
 sencha.bootstrapCore();
 
 Ext.Loader.setPath('NoSQL', __dirname + '/../lib/NoSQL');
 
-var model = Ext.create('NoSQL.model.Document', {
-    'name' : 'XC'
+
+var store = new Ext.data.Store({
+	model: 'NoSQL.model.Document',
+	autoLoad: true
 });
-
+	
+var model = Ext.create('NoSQL.model.Document', {
+    'name' : 'XC',
+    'id'   : 3
+});
 model.save();
-
-var store = new Ext.data.Store({model: 'NoSQL.model.Document'});
-console.log(store.load());
+console.log(store.load().data.items);
