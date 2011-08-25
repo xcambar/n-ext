0f0c725560bd970212bd5175ed4fa112da26b3ab
Xavier Cambar <xavier.cambar@inovia14.(none)>
2011-08-25 19:19:56 +0200

    Modified the file structure to make it easier

--- examples/bootstrap.js
+++ examples/bootstrap.js
@@ -6,8 +6,8 @@
 require.paths.unshift('/usr/local/lib/node_modules');
 var sencha = require('n-ext');
 sencha.bootstrapCore();
+
+
 //That's all that is required to be able to use the data package of ExtJS in a node environment
-
-
 Ext.require('NoSQL.model.Document');
 var d = Ext.create('NoSQL.model.Document');
