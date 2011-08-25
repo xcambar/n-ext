5a71cf6caf0f3a298403ce28a31030d09df8a3f5
Xavier Cambar <xavier.cambar@inovia14.(none)>
2011-08-25 16:32:53 +0200

    Updated Unit Tests

--- tests/testNExt.js
+++ tests/testNExt.js
@@ -1,35 +1,41 @@
-require.paths.unshift('./lib');
-
-
-//Ext.require('NoSQL.model.Document');
-
-exports.testExtCanSandBox = function(test) {
+exports['Ext can be unset by the module'] = function(test){
 	test.expect(1);
 	var sencha = require('n-ext'); 
-	var e = sencha.bootstrapCore(true);
-	test.ok(typeof(Ext) === 'undefined', 'Global variable Ext should not be defined');
+	sencha.bootstrapCore();
+	sencha.unset();
+	test.ok(typeof(Ext) === 'undefined');
 	test.done();
-}
+};
 
-exports.testSandBoxMustAccessExtInternally = function(test) {
-	test.expect(1);
+exports['Ext can be unset and reset safely'] = function(test) {
+	test.expect(3);
 	var sencha = require('n-ext'); 
-	var e = sencha.bootstrapCore(true);
-	e.apply({}, {});
-	test.ok(true);
+	sencha.bootstrapCore();
+	test.ok(typeof(Ext) !== 'undefined');
+	sencha.unset();
+	test.ok(typeof(Ext) === 'undefined');
+	sencha.bootstrapCore();
+	test.ok(typeof(Ext) !== 'undefined', 'Global variable Ext should be defined');
 	test.done();
 }
-
-exports.testExtCanBeGlobal = function(test) {
+/**
+**/
+exports['Namespaces can not be loaded if path is undefined'] = function(test) {
+	test.expect(1);
+	var sencha = require('n-ext');
+	sencha.bootstrapCore();
+	test.throws(function() {
+		Ext.require('Test.Class');
+	});
+	sencha.unset();
+	test.done();
+}
+exports['Ext is globally defined'] = function(test) {
 	test.expect(1);
 	var sencha = require('n-ext'); 
 	sencha.bootstrapCore();
 	test.ok(typeof(Ext) !== 'undefined', 'Global variable Ext should be defined');
 	test.done();
 }
-
-exports.testCanRequireExistingExtClasses = function(test){
-    test.expect(1);
-    test.ok(true, "this assertion should pass");
-    test.done();
-};
+/**
+**/
